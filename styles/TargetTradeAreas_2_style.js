20d393a67255da5aba79d60f82dedd48baa4f0e0
cbtl-americas-hq <136393184+cbtl-americas-hq@users.noreply.github.com>
2024-08-13 19:49:47 -0700

    Add files via upload

---
+++ styles/TargetTradeAreas_2_style.js
@@ -0,0 +1,41 @@
+var size = 0;
+var placement = 'point';
+function categories_TargetTradeAreas_2(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                switch(value.toString()) {case 'viable':
+                    return [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 8.8 + size, points: 4,
+            stroke: new ol.style.Stroke({color: 'rgba(61,128,53,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1}), fill: new ol.style.Fill({color: 'rgba(84,176,74,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_TargetTradeAreas_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("decision");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+var style = categories_TargetTradeAreas_2(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement);
+
+    return style;
+};
